39990af4f4ac6c635522ad2d44c6b36dca975e0b
Duje Marušić <duje.marusic@q-software.com>
2021-08-23 12:36:01 +0200

    Small margin fix

--- src/app/services/contact/contact.service.ts
+++ src/app/services/contact/contact.service.ts
@@ -16,15 +16,21 @@ export class ContactService {
 
   getContact(id: number) {
     this.contacts = JSON.parse(localStorage.getItem('contacts'));
+
     if (!this.contacts) return;
+
     const contact = this.contacts.filter(c => c.id == id)[0];
+
     return contact;
   }
 
   createContact(contact: Contact) {
     this.contacts = JSON.parse(localStorage.getItem('contacts'));
+
     if (!this.contacts) this.contacts = [];
+
     this.contacts.push(contact);
+
     localStorage.setItem('contacts', JSON.stringify(this.contacts));
   }
 
@@ -40,13 +46,17 @@ export class ContactService {
 
   deleteContact(contactId: number) {
     this.contacts = JSON.parse(localStorage.getItem('contacts'))
+
     this.contacts = this.contacts.filter(c => c.id != contactId);
+    
     localStorage.setItem('contacts', JSON.stringify(this.contacts));
   }
 
   getUserId() {
     this.contacts = JSON.parse(localStorage.getItem('contacts'))
+
     if (!this.contacts) return 1;
+
     return this.contacts[this.contacts.length - 1].id + 1; 
   }
 }
